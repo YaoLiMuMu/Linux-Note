2fd3557806eadbe77fe9ddebca93be8afd37cfca
YaoLiMuMu <yl347522772@gmail.com>
2021-06-01 21:48:49 +0800

    feat bms_resolution.py: CST message period calculate, CTS parse
    calculate CST period(mean, max, min, std, cv), parse CTS date and time
    
    Close #321

--- bms_resolution.py
+++ bms_resolution.py
@@ -4,7 +4,7 @@
 # @Auther : yaolimumu
 # @Site : Nebula
 # @File : bms_resolution.py
-# @Version: V0.0.1
+# @Version: V0.0.2
 # @Software : Python3
 
 # bms message(.csv files) automatic analysis script
@@ -100,6 +100,7 @@ triple_variable = ('SYS_MS', 'MAKE_CAN_ID(HEX)', 'DATA(HEX)')
 BHM_FrameID = '*182756f4*'  #[OK]
 BCL_FrameID = '*181056f4*'  #[OK]
 CCS_FrameID = '*1812f456*'  #[OK]
+CTS_FrameID = '*1807f456*'
 CML_FrameID = '*1808f456*'  #[OK]
 BRO_FrameID = '*100956f4*'  #[OK]
 CRO_FrameID = '*100af456*'  #[OK]
@@ -123,7 +124,7 @@ SAJ1939_CTS_FrameID = '*1ceb56f4*'
 SAJ1939_EndofMsgAck = '*1cecf456*'
 os.makedirs('csvfiles', exist_ok=True) # make directory to store analysis results
 # Loop through every file in the current working directory
-print('请将解析工具放在.csv文件同目录下, 生成结果与图形自动保存在同目录的csvfiles文件中...')
+print('请将解析工具放在.csv文件同目录下, 解析结果与图形自动保存在同目录的csvfiles文件中...')
 print('注意!!!csv文件需要包含表头:', triple_variable)
 csvfiles = os.listdir(path)
 csvfiles.sort() #排序
@@ -138,20 +139,23 @@ for file in csvfiles:
         framebuffer = pd.read_csv(file, usecols=[triple_variable[0], triple_variable[1], triple_variable[2]])
     framebuffer.insert(3,'TEXT1', '') # add a column as TEXT1
     framebuffer['TEXT2'], framebuffer['TEXT3'], framebuffer['Fault'] = ['', '', ''] # add columns as TEXT2, TEXT3 = np.NaN
+    framebuffer = framebuffer.dropna(how='any', axis=0) # delete/drop 'NaN' 
     buffer= np.array(framebuffer)
     data=[]
-    data.append([triple_variable[0], triple_variable[1], triple_variable[2], 'TEXT1', 'TEXT2', 'TEXT3', 'TEXT4']) # add csv header
+    data.append([triple_variable[0], triple_variable[1], triple_variable[2], 'TEXT1', 'TEXT2', 'TEXT3', 'Fault']) # add csv header
     BCL_Axis = [[], [], []]     # [0] timestamp [1] voltage [2] current
     CCS_Axis = [[], [], []]     # [0] timestamp [1] voltage [2] current
     MsgBcpDict = [0, 0, 0] # [0] RTS [1] donestep [2] long connect join temp
     MsgBrmDict = [0, 0, 0]
     MsgBcsDict = [0, 0, 0]
+    CST_Period = []
+    Not_First_CST = False
     stopStamp = False
     TimeoutFlag = False
     for item in buffer:
         sh = item[0]
-        if match(sh, '*Gun*'):
-            continue    # filter 'can:0 Gun:00' row
+        # if match(sh, '*Gun*'):
+        #     continue    # filter 'can:0 Gun:00' row
         if match(sh, MsgDateTime): # filter DateTime
             sh = item[1]
             if match(sh.lower(), BCL_FrameID): # filter BCL messages
@@ -161,9 +165,9 @@ for file in csvfiles:
                 BCL_Axis[2].append(BCLCurrent)
                 BCL_Axis[1].append(BCLVoltage)
                 if match(file.lower(), '*bms*'):
-                    BCL_Axis[0].append(datetime.datetime.strptime(timestamp,'%Y-%m-%d %H:%M:%S'))
+                    BCL_Axis[0].append(datetime.datetime.strptime(timestamp,'%Y-%m-%d %H:%M:%S').time())
                 else:
-                    BCL_Axis[0].append(datetime.datetime.strptime(timestamp,'%H:%M:%S.%f'))
+                    BCL_Axis[0].append(datetime.datetime.strptime(timestamp,'%H:%M:%S.%f').time())
                 item[3] = 'BCL:需求电压=' + str(BCLVoltage) + 'V'
                 item[4] = 'BCL:需求电流=' + str(BCLCurrent) + 'A'
             elif match(sh.lower(), CCS_FrameID): # filter CCS messages
@@ -173,9 +177,9 @@ for file in csvfiles:
                 CCS_Axis[2].append(CCSCurrent)
                 CCS_Axis[1].append(CCSVoltage)
                 if match(file.lower(), '*bms*'):
-                    CCS_Axis[0].append(datetime.datetime.strptime(timestamp,'%Y-%m-%d %H:%M:%S'))
+                    CCS_Axis[0].append(datetime.datetime.strptime(timestamp,'%Y-%m-%d %H:%M:%S').time())
                 else:
-                    CCS_Axis[0].append(datetime.datetime.strptime(timestamp,'%H:%M:%S.%f'))
+                    CCS_Axis[0].append(datetime.datetime.strptime(timestamp,'%H:%M:%S.%f').time())
                 item[3] = 'CCS: U=' + str(CCSVoltage) + 'V' 
                 item[4] = 'CCS: I=' + str(CCSCurrent) + 'A'
             elif match(sh.lower(), BHM_FrameID): # filter BHM messages
@@ -281,6 +285,12 @@ for file in csvfiles:
                     item[3] = '车端主动中止->>'
                     stopStamp = True
             elif match(sh.lower(), CST_FrameID):
+                if not match(file.lower(), '*bms*'):
+                    if Not_First_CST:
+                        GetAfterTime = datetime.datetime.strptime(str(item[0]).strip('[]'),'%H:%M:%S.%f')
+                        CST_Period.append((GetAfterTime - GetBeforeTime).microseconds/1000 + (GetAfterTime - GetBeforeTime).seconds*1000)
+                    GetBeforeTime = datetime.datetime.strptime(item[0].strip('[]'),'%H:%M:%S.%f')
+                    Not_First_CST = True
                 item[4] = 'CST:中止原因' + CST_Parser(item[2])
                 if stopStamp == False:
                     item[3] = '桩端主动中止...'
@@ -314,7 +324,20 @@ for file in csvfiles:
                 if TimeoutFlag == False:
                     item[3] = '桩端发现超时重连中...'
                     TimeoutFlag == True
+            elif match(sh.lower(), CTS_FrameID):
+                item[3] = 'CTS:同步时间...' 
+                item[4] = 'CTS:' + item[2][18:20] + item[2][15:17] + '-' + item[2][12:14] + '-' + item[2][9:11] + ' ' + item[2][6:8]  + ':' + item[2][3:5] + ':' + item[2][0:2]
             data.append(item)
+    # print(round(np.mean(CST_Period),1), np.maximum(CST_Period), np.std(CST_Period), np.min(CST_Period))
+    data.append([])
+    data.append(['Message Type', 'Period(ms)', 'mean', 'max', 'min', 'std', 'cv'])
+    if (not match(file.lower(), '*bms*')) and Not_First_CST :
+        calcu_mean = round(np.mean(CST_Period), 1)
+        calcu_min = np.min(CST_Period)
+        calcu_max = np.max(CST_Period)
+        calcu_std = round(np.std(CST_Period), 1)
+        calcu_cv = round(np.std(CST_Period)/np.mean(CST_Period), 1)
+        data.append(['CST', '10', str(calcu_mean), str(calcu_max), str(calcu_min), str(calcu_std), str(calcu_cv)])
     csvFilesobj = open(os.path.join('csvfiles', file), 'w', newline='') # create copy file in csvfiles direstory
     csvWriter = csv.writer(csvFilesobj)
     for row in data:
@@ -327,7 +350,6 @@ for file in csvfiles:
     # plt.plot(CCS_Axis[0], CCS_Axis[1], color='green', label='CCS Voltage')
     # plt.plot(CCS_Axis[0], CCS_Axis[2], color='blue', label='CCS Current')
     # plt.plot(BCL_Axis[0], BCL_Axis[2], color='black', label='BCL Current')
-
     ChangingFigure = plt.figure()
     DoubleAxis = ChangingFigure.add_subplot(111)
     DoubleAxis.plot(BCL_Axis[0], BCL_Axis[1], color='red', label='BCL Voltage')
